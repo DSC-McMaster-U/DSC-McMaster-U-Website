1c3adb682df3ca2cd470911f972f86e078686107
Kytha <thatchek@mcmaster.ca>
2020-10-13 15:53:48 -0400

    Changed color definitons.

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,56 +1,70 @@
 import React from "react";
 import PropTypes from "prop-types";
-import SplashParticle from './splashParticle';
-
+import SplashParticle from "./splashParticle";
+import { blue, red, yellow, green } from "../asset/colors";
 import SplashGraph from "./../asset/image/splash.png";
 
 const Splash = ({ title, subTitle }) => (
-    <div className="lg:container md:container sm:container">
-
-        {/* splash content */}
-        <div className="grid lg:grid-cols-2 sm:grid-cols-1">
-            {/* splash text */}
-            <div>
-                <h2>{title}</h2>
-                <p>{subTitle}</p>
-                <div className="grid grid-cols-2">
-                    <div>
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="https://docs.google.com/forms/d/e/1FAIpQLScjcm8b9ay0-JFcANSH_-A19zO7-KZK40_ppwO2Pno88eTh7A/viewform">
-                            join us
-                        </button>
-                    </div>
-                    <div>
-                        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                            contact us
-                        </button>
-                    </div>
-                </div>
-            </div>
-            {/* splash graphic image */}
-            <div>
-                <div className="z-10"><SplashParticle /></div>
-                <div className="z-0"><img src= {SplashGraph}/></div>
-            </div>
+  <div className="lg:container md:container sm:container">
+    {/* splash content */}
+    <div className="grid lg:grid-cols-2 sm:grid-cols-1">
+      {/* splash text */}
+      <div>
+        <h2>{title}</h2>
+        <p>{subTitle}</p>
+        <div className="grid grid-cols-2">
+          <div>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              href="https://docs.google.com/forms/d/e/1FAIpQLScjcm8b9ay0-JFcANSH_-A19zO7-KZK40_ppwO2Pno88eTh7A/viewform"
+            >
+              join us
+            </button>
+          </div>
+          <div>
+            <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+              contact us
+            </button>
+          </div>
         </div>
-
-        {/* splash footer */}
-        <div className="grid grid-cols-4">
-            <div><hr style={{backgroundColor: `#1967D2`, height: `2px`}}/></div>
-            <div><hr style={{backgroundColor: `#C5221F`, height: `2px`}}/></div>
-            <div><hr style={{backgroundColor: `#F29900`, height: `2px`}}/></div>
-            <div><hr style={{backgroundColor: `#188038`, height: `2px`}}/></div>
+      </div>
+      {/* splash graphic image */}
+      <div>
+        <div className="z-10">
+          <SplashParticle />
         </div>
+        <div className="z-0">
+          <img src={SplashGraph} />
+        </div>
+      </div>
     </div>
-  );
 
-  Splash.propTypes = {
-    title: PropTypes.string,
-    subTitle: PropTypes.string
-  };
-  
-  Splash.defaultProps = {
-    title: ``,
-    subTitle: ``
-  };
-  
-  export default Splash;
+    {/* splash footer */}
+    <div className="grid grid-cols-4">
+      <div>
+        <hr style={{ backgroundColor: blue[2], height: `2px` }} />
+      </div>
+      <div>
+        <hr style={{ backgroundColor: red[2], height: `2px` }} />
+      </div>
+      <div>
+        <hr style={{ backgroundColor: yellow[2], height: `2px` }} />
+      </div>
+      <div>
+        <hr style={{ backgroundColor: green[2], height: `2px` }} />
+      </div>
+    </div>
+  </div>
+);
+
+Splash.propTypes = {
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
+};
+
+Splash.defaultProps = {
+  title: ``,
+  subTitle: ``,
+};
+
+export default Splash;
--- src/components/splashParticle.js
+++ src/components/splashParticle.js
@@ -1,37 +1,38 @@
 import React from "react";
-
-import Particles from 'react-particles-js';
+import { red, yellow, green, blue } from "../asset/colors";
+import Particles from "react-particles-js";
 
 const SplashParticle = () => (
-    <Particles
-        params={{
-	        "particles": {
-	            "number": {
-	                "value": 10
-                },
-                "color" : {
-                    "value": ["#FBBC04", "#4285F4", "#34A853", "#EA4335"]
-                },
-	            "size": {
-	                "value": 10
-                },
-                // "line_linked": {
-                //     "enable": true,
-                //     "distance": 150,
-                //     "color": "#c8c8c8",
-                //     "opacity": 0.4,
-                //     "width": 1
-                // },
-            },
-	        "interactivity": {
-	            "events": {
-	                "onhover": {
-	                    "enable": true,
-	                    "mode": "repulse"
-	                }
-	            }
-	        }
-	    }} />
+  <Particles
+    params={{
+      particles: {
+        number: {
+          value: 10,
+        },
+        color: {
+          value: [yellow[4], blue[4], green[4], red[4]],
+        },
+        size: {
+          value: 10,
+        },
+        // "line_linked": {
+        //     "enable": true,
+        //     "distance": 150,
+        //     "color": "#c8c8c8",
+        //     "opacity": 0.4,
+        //     "width": 1
+        // },
+      },
+      interactivity: {
+        events: {
+          onhover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+      },
+    }}
+  />
 );
 
 export default SplashParticle;
