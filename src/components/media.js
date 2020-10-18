a9478c748ddfa6a0b4257ca02e59ccbe9a3a8e44
edwin-do <doedwin123@gmail.com>
2020-10-18 19:44:00 -0400

    change image name

--- src/components/media.js
+++ src/components/media.js
@@ -5,7 +5,7 @@ import linkedinLogo from "../images/icons8-linkedin-50.png";
 import githubLogo from "../images/icons8-github-50.png";
 import twitterLogo from "../images/icons8-twitter-50.png";
 import instaLogo from "../images/icons8-instagram-50.png";
-import freepik from "../images/media_freepik.jpg";
+import announce_freepik from "../images/media_freepik.jpg";
 
 function Media(){
 
@@ -13,34 +13,34 @@ function Media(){
 
     return (
         <section className="relative">
-                <div className="container mx-auto">
-                    <div class="flex">
-                        <div class="w-1/6 mt-20">
-                            <h1>Follow Us</h1>
-                            <p className="mb-2 ml-5"><img style={logoStyle} src={fbLogo} alt="facebook_logo" /> DSC_McMaster</p>
-                            <p className="mb-2 ml-5"><img style={logoStyle} src={linkedinLogo} alt="linkedin_logo" /> DSC_McMaster</p>
-                            <p className="mb-2 ml-5"><img style={logoStyle} src={discordLogo} alt="discord_logo" /> DSC_McMaster</p>
-                            <p className="mb-2 ml-5"><img style={logoStyle} src={githubLogo} alt="github_logo" /> DSC_McMaster</p>
-                            <p className="mb-2 ml-5"><img style={logoStyle} src={instaLogo} alt="instagram_logo" /> DSC_McMaster</p>
-                            <p className="mb-2 ml-5"><img style={logoStyle} src={twitterLogo} alt="twitter_logo" /> DSC_McMaster</p>
-                        </div>
-                        <div class="w-5/6 ml-5">
-                            <div class="flex">
-                                <div class="w-1/2 mt-0">
-                                    <img className="min-h-50" src={freepik} alt="Designed by slidesgo / Freepik"></img>
-                                </div>
-                                <div class="w-1/2 mt-20">
-                                    <h1>Stay Connected</h1>
-                                    <h3 className="text-black text-opacity-50">Get the latest updates</h3>
-                                    <div className="shadow-xl px-5 py-5">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                        Tellus cras adipiscing enim eu turpis egestas pretium aenean. Sodales neque sodales ut etiam. Pulvinar neque laoreet suspendisse interdum consectetur libero id. 
-                                    </div>
+            <div className="container mx-auto">
+                <div class="flex">
+                    <div class="w-1/6 mt-20">
+                        <h1>Follow Us</h1>
+                        <p className="mb-2 ml-5"><img style={logoStyle} src={fbLogo} alt="facebook_logo" /> DSC_McMaster</p>
+                        <p className="mb-2 ml-5"><img style={logoStyle} src={linkedinLogo} alt="linkedin_logo" /> DSC_McMaster</p>
+                        <p className="mb-2 ml-5"><img style={logoStyle} src={discordLogo} alt="discord_logo" /> DSC_McMaster</p>
+                        <p className="mb-2 ml-5"><img style={logoStyle} src={githubLogo} alt="github_logo" /> DSC_McMaster</p>
+                        <p className="mb-2 ml-5"><img style={logoStyle} src={instaLogo} alt="instagram_logo" /> DSC_McMaster</p>
+                        <p className="mb-2 ml-5"><img style={logoStyle} src={twitterLogo} alt="twitter_logo" /> DSC_McMaster</p>
+                    </div>
+                    <div class="w-5/6 ml-5">
+                        <div class="flex">
+                            <div class="w-1/2 mt-0">
+                                <img className="min-h-50" src={announce_freepik} alt="Designed by slidesgo / Freepik"></img>
+                            </div>
+                            <div class="w-1/2 mt-20">
+                                <h1>Stay Connected</h1>
+                                <h3 className="text-black text-opacity-50">Get the latest updates</h3>
+                                <div className="rounded-lg shadow-xl px-5 py-5">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Tellus cras adipiscing enim eu turpis egestas pretium aenean. Sodales neque sodales ut etiam. Pulvinar neque laoreet suspendisse interdum consectetur libero id. 
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
          </section>
     )
 }
