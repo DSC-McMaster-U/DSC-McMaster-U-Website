ba34e5575f4547019d5246f2c431bf62a3df3a87
edwin-do <doedwin123@gmail.com>
2020-10-26 23:48:40 -0400

    add href placeholders

--- src/components/media.js
+++ src/components/media.js
@@ -20,42 +20,42 @@ function Media(){
                     <div className="w-2/5 sm:1/6 lg:w-1/4">
                         <h1>Follow Us</h1>
                         <div className={mediaLinkStyle}>
-                            <a className="ml-5">
+                            <a href="#" className="ml-5">
                                 <img className={logoStyle} src={fbLogo} alt="facebook_logo" /> DSC_McMaster
                             </a>
                         </div>
                         <div className={mediaLinkStyle}>
-                            <a className="ml-5">
+                            <a href="#" className="ml-5">
                                 <img className={logoStyle} src={linkedinLogo} alt="linkedin_logo" /> DSC_McMaster
                             </a>
                         </div>
                         <div className={mediaLinkStyle}>
-                            <a className="ml-5">
+                            <a href="#" className="ml-5">
                                 <img className={logoStyle} src={discordLogo} alt="discord_logo" /> DSC_McMaster
                             </a>
                         </div>
                         <div className={mediaLinkStyle}>
-                            <a className="ml-5">
+                            <a href="#" className="ml-5">
                                 <img className={logoStyle} src={githubLogo} alt="github_logo" /> DSC_McMaster
                             </a>
                         </div>
                         <div className={mediaLinkStyle}>
-                            <a className="ml-5">
+                            <a href="#" className="ml-5">
                                 <img className={logoStyle} src={instaLogo} alt="instagram_logo" /> DSC_McMaster
                             </a>
                         </div>
                         <div className={mediaLinkStyle}>
-                            <a className="ml-5">
-                            <img className={logoStyle} src={twitterLogo} alt="twitter_logo" /> DSC_McMaster
+                            <a href="#" className="ml-5">
+                                <img className={logoStyle} src={twitterLogo} alt="twitter_logo" /> DSC_McMaster
                             </a>
                         </div>
                     </div>
-                    <div class="w-3/5 sm:5/6 lg:w-3/4">
+                    <div className="w-3/5 sm:5/6 lg:w-3/4">
                         <div className="flex">
-                            <div class="w-0 lg:w-1/2">
+                            <div className="w-0 lg:w-1/2">
                                 <img className={imageStyle} src={announce_freepik} alt="Designed by slidesgo/Freepik"></img>
                             </div>
-                            <div class="w-auto lg:w-1/2 ml-10 sm:ml-0">
+                            <div className="w-auto lg:w-1/2 ml-10 sm:ml-0">
                                 <h1>Stay Connected</h1>
                                 <h3 className="text-black text-opacity-50">Get the latest updates</h3>
                                 <div className="hidden sm:block rounded-lg shadow-xl px-5 py-5">
