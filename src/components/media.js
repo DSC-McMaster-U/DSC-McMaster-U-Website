c5db520c33b9e41911d04f47e88dd8a47fed4bb6
edwin-do <doedwin123@gmail.com>
2020-10-26 23:42:37 -0400

    add responsiveness

--- src/components/media.js
+++ src/components/media.js
@@ -9,32 +9,58 @@ import announce_freepik from "../images/media_freepik.jpg";
 
 function Media(){
 
-    const logoStyle = {height: "20%", width: "20%", display: "inline-block"};
+    const logoStyle = "h-6 w-h md:h-8 inline-block mb-0";
+    const imageStyle = "hidden lg:block lg:h-250";
+    const mediaLinkStyle = "my-1 inline-block";
 
     return (
-        <section className="relative">
+        <section className="relative mt-20 mb-10">
             <div className="container mx-auto">
-                <div class="flex">
-                    <div class="w-1/6 mt-20">
+                <div className="flex">
+                    <div className="w-2/5 sm:1/6 lg:w-1/4">
                         <h1>Follow Us</h1>
-                        <p className="mb-2 ml-5"><img style={logoStyle} src={fbLogo} alt="facebook_logo" /> DSC_McMaster</p>
-                        <p className="mb-2 ml-5"><img style={logoStyle} src={linkedinLogo} alt="linkedin_logo" /> DSC_McMaster</p>
-                        <p className="mb-2 ml-5"><img style={logoStyle} src={discordLogo} alt="discord_logo" /> DSC_McMaster</p>
-                        <p className="mb-2 ml-5"><img style={logoStyle} src={githubLogo} alt="github_logo" /> DSC_McMaster</p>
-                        <p className="mb-2 ml-5"><img style={logoStyle} src={instaLogo} alt="instagram_logo" /> DSC_McMaster</p>
-                        <p className="mb-2 ml-5"><img style={logoStyle} src={twitterLogo} alt="twitter_logo" /> DSC_McMaster</p>
+                        <div className={mediaLinkStyle}>
+                            <a className="ml-5">
+                                <img className={logoStyle} src={fbLogo} alt="facebook_logo" /> DSC_McMaster
+                            </a>
+                        </div>
+                        <div className={mediaLinkStyle}>
+                            <a className="ml-5">
+                                <img className={logoStyle} src={linkedinLogo} alt="linkedin_logo" /> DSC_McMaster
+                            </a>
+                        </div>
+                        <div className={mediaLinkStyle}>
+                            <a className="ml-5">
+                                <img className={logoStyle} src={discordLogo} alt="discord_logo" /> DSC_McMaster
+                            </a>
+                        </div>
+                        <div className={mediaLinkStyle}>
+                            <a className="ml-5">
+                                <img className={logoStyle} src={githubLogo} alt="github_logo" /> DSC_McMaster
+                            </a>
+                        </div>
+                        <div className={mediaLinkStyle}>
+                            <a className="ml-5">
+                                <img className={logoStyle} src={instaLogo} alt="instagram_logo" /> DSC_McMaster
+                            </a>
+                        </div>
+                        <div className={mediaLinkStyle}>
+                            <a className="ml-5">
+                            <img className={logoStyle} src={twitterLogo} alt="twitter_logo" /> DSC_McMaster
+                            </a>
+                        </div>
                     </div>
-                    <div class="w-5/6 ml-5">
-                        <div class="flex">
-                            <div class="w-1/2 mt-0">
-                                <img className="min-h-50" src={announce_freepik} alt="Designed by slidesgo / Freepik"></img>
+                    <div class="w-3/5 sm:5/6 lg:w-3/4">
+                        <div className="flex">
+                            <div class="w-0 lg:w-1/2">
+                                <img className={imageStyle} src={announce_freepik} alt="Designed by slidesgo/Freepik"></img>
                             </div>
-                            <div class="w-1/2 mt-20">
+                            <div class="w-auto lg:w-1/2 ml-10 sm:ml-0">
                                 <h1>Stay Connected</h1>
                                 <h3 className="text-black text-opacity-50">Get the latest updates</h3>
-                                <div className="rounded-lg shadow-xl px-5 py-5">
+                                <div className="hidden sm:block rounded-lg shadow-xl px-5 py-5">
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                    Tellus cras adipiscing enim eu turpis egestas pretium aenean. Sodales neque sodales ut etiam. Pulvinar neque laoreet suspendisse interdum consectetur libero id. 
+                                    Tellus cras adipiscing enim eu turpis egestas pretium aenean. Sodales neque sodales ut etiam.
                                 </div>
                             </div>
                         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,15 @@ import Layout from "../layouts";
 import SEO from "../components/seo";
 import Media from '../components/media';
 
-const IndexPage = () => (
-  // <Layout>
-  //   <SEO title="Home" />
-  //   <h1>Hello World!</h1>
-  //   <p>Welcome to the site.</p>
-  // </Layout>
-  <Media/>
-);
+const IndexPage = () => {
+  return(
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hello World!</h1>
+      <p>Welcome to the site.</p>
+      <Media/>
+    </Layout>
+  );
+};
 
 export default IndexPage;
