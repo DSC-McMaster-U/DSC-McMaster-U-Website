dcd9a0f3302b4d71628207905e43f5e957cf0ec4
Tasmiha Hassan <htasmiha@gmail.com>
2020-10-26 21:45:10 -0400

    final implementation of Events

--- src/components/events.js
+++ src/components/events.js
@@ -1,9 +1,13 @@
 import PropTypes from "prop-types";
 import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
-import Img from "gatsby-image";
+import Img from "gatsby-image/withIEPolyfill";
 import Button from "./Button"
+import EventData from "../content/events.json"
+
 function Events ({ siteTitle }) {
+  const maxEvents = 3
+  let eventCounter = 0
   const data = useStaticQuery(graphql`
   query ProfilePic {
     image: file(relativePath: {eq: "events.png"}) {
@@ -21,11 +25,11 @@ function Events ({ siteTitle }) {
   `)
   return (
     <section className="relative" >
-      <div className="container mx-auto">
+      <div className="container mx-auto"> 
         <div className="border-t-4 border-blue-400">
           <div className="border-b-4 border-gray-200">
-            <div className="items-center flex flex-wrap px-8">
-              <div className="w-6/12 h-full py-8">
+            <div className="flex flex-wrap px-8">
+              <div className="w-6/12 h-full pt-8">
                 <div className="text-4xl">
                   Events & Workshops
                 </div>
@@ -37,13 +41,54 @@ function Events ({ siteTitle }) {
                     View Events
                   </Button>
                 </a>
-              </div>
-              <Img fluid={data.image.childImageSharp.fluid}  className="bg-auto" style={{ height: "50%", width: "50%", position: "relative", top: 0, right: 0}} />
-              {/* <div className="w-6/12 py-8">
-                <div className="rounded-lg bg-coreGray text-white py-2 px-2 text-xs">
-                  Upcoming Events
-                </div>
-              </div> */}
+                <Img fixed={data.image.childImageSharp.fixed} alt="" objectFit="cover" className="" objectPosition="100% 100%" />           
+          </div>
+          <div className="py-8 w-6/12">
+            <div className="pl-8">
+                <span className="rounded-lg bg-gray-500 text-white py-2 px-2 text-xs">
+                    Upcoming Events
+                </span>
+            </div>
+            <ul className="pt-8 pl-8">
+                {EventData.filter(event => {
+                    const currentTime = new Date()
+                    const eventTime = new Date(event.jsDate)
+                    if (eventTime > currentTime && eventCounter<maxEvents){
+                        eventCounter++
+                        return true
+                    }
+                    return false
+                }).map((event,i) => {
+                    return (<li className={ i === 0 ? "mx-auto" : "mx-auto pt-12"}>
+                    <div className="flex flex-row">
+                        <div>
+                            <img 
+                                src={require("../images/dscIcon.png")}
+                                alt="DSC Icon"
+                            />
+                        </div>
+                        <a href={event.link} className="pl-2 hover:underline"> 
+                        {event.title}
+                        </a>
+                        </div>
+                    <div className="flex flex-row pl-10 pt-2">
+                        <span className="text-gray-500 mr-2 text-xs">
+                            <i className="fill-current far fa-calendar-alt text-blue-400 fa-lg pt-2 pr-2"></i>
+                            {event.date}
+                        </span>
+                        <span className="text-gray-500 mr-2 text-xs">
+                            <i className="fill-current far fa-clock text-green-400 fa-lg pt-2 pr-2"></i>
+                            {event.time}
+                        </span>
+                        <span className="text-gray-500 mr-2 text-xs">
+                            <i className="fill-current fas fa-map-marker-alt text-red-400 fa-lg pt-2 pr-2"></i>
+                            {event.location}
+                        </span>
+                    </div>
+                </li>)
+                })}  
+            </ul>
+            </div>
             </div>
           </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Layout from "../layouts";
 import SEO from "../components/seo";
 import Events from "../components/events"
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const IndexPage = () => {
   return (
