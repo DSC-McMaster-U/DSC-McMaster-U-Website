426f3f0a585af112197516fafafd80b5abc5e77e
DhrumilP <dhrumil172@gmail.com>
2020-11-10 09:08:21 -0500

    Added randomize color change to the view wiki button

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,9 @@ import React from "react";
 import About_Card from "./About_Card";
 
 const About = () => {
+  const colorTray = ["red", "blue", "green", "yellow"];
+  let oneColor = colorTray[Math.floor(Math.random() * colorTray.length)];
+
   return (
     <div className="border-t-2 border-top-about container mx-auto border-bottom-about my-6">
       <section className="my-6 grid grid-rows-5 md:grid-rows-1 md:grid-cols-10 py-6">
@@ -20,7 +23,9 @@ const About = () => {
           <div className="grid grid-cols-3">
             <div className="col-span-2">
               <a href="https://www.notion.so/DSC-McMasterU-Wiki-f39c9ea5e7b141fba08cfc01588d92c4">
-                <button className="wiki bg-transparent text-lg hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-3 px-6 border border-blue-500 hover:border-transparent rounded-md my-5 outline-none">
+                <button
+                  className={`wiki bg-transparent text-lg text-blue-700 font-semibold hover:text-white hover:bg-${oneColor}-400 py-3 px-6 border border-blue-500 hover:border-transparent rounded-md my-5 outline-none`}
+                >
                   View Wiki
                 </button>
               </a>
