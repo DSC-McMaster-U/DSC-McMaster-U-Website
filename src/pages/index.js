7d86bc5e7c1ec05a153e29c68275c2d8b0485a84
Tasmiha Hassan <htasmiha@gmail.com>
2020-11-09 19:28:08 -0500

    Tas/002 (#3)
    * Minimal Viable Product of Events
    
    * final implementation of Events
    
    * Fixed image responsiveness
    
    * Applied all pull request recommendations
    
    * Remove section padding on smaller screens
    
    Co-authored-by: Kytha <thatchek@mcmaster.ca>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Layout from "../layouts";
 import SEO from "../components/seo";
+import Events from "../components/Events";
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
       <h1>Hello World!</h1>
-      <p>Welcome to the site.</p>      
+      <p>Welcome to the site.</p>
+      <Events />
     </Layout>
   );
 };
