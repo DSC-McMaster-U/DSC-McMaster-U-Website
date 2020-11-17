74a9ade1f6d8b115a4a4dc88060b7dc2c6b10b5d
Kyle Thatcher <33584092+Kytha@users.noreply.github.com>
2020-11-17 12:47:25 -0500

    Tweak Splash Section (#8)
    * Alternate figma image for splash. Make particles responsive
    
    * Tweak particle config and styles.
    
    * Reorganize splash files
    
    * Remove unnecessary props

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Layout from "../layouts";
 import SEO from "../components/Seo";
-import Splash from "../components/Splash";
+import Splash from "../components/Splash/";
 import Events from "../components/Events";
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <Splash subTitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tortor ligula, hendrerit eu turpis id, feugiat mattis nulla. Integer pretium, sem id vestibulum vestibulum, magna est elementum mauris, id sollicitudin eros libero vitae enim." />
+      <Splash />
       <Events />
     </Layout>
   );
