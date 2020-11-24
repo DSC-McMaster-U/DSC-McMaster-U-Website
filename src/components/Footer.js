2d147d4d4b76aedf628aaa46ad34c3e5db12cf64
edwin-do <doedwin123@gmail.com>
2020-11-24 02:25:52 -0500

    add basic responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,46 +1,41 @@
 import React from "react";
 
+const headingStyle = "px-4 py-2 text-lg text-gray-800 font-semibold";
+const linkStyle = "text-gray-600 px-4 py-2 text-sm";
+const iconStyle = "text-xl";
+
 function Footer() {
   return (
     <footer>
-      <div className="table w-full mt-32 mb-10">
-        <div className="table-footer-group">
-          <div className="table-row text-base text-gray-900 font-semibold">
-            <div className="table-cell px-4 py-2">
-              Feel free to reach out to us!
+      <div className="grid grid-cols-1 gap-2 mt-32 text-center sm:grid-cols-3 ">
+        <div className="mt-8">
+          <span className={headingStyle}>Contact Us</span>
+          <div className="grid grid-cols-1">
+            <div className={linkStyle}>© 2020 | All rights reserved</div>
+            <div className={linkStyle}>
+              Reach out to us at dscmcmaster@email.ca
             </div>
-            <div className="table-cell px-4 py-2">Programs</div>
-            <div className="table-cell px-4 py-2">External links</div>
-          </div>
-          <div className="table-row">
-            <div className="table-cell text-gray-700 px-4 py-2 text-sm">
-              dscmcmaster@email.ca
-            </div>
-            <div className="table-cell text-gray-700 px-4 py-2 text-sm">
-              Program1
-            </div>
-            <div className="table-cell text-gray-700 px-4 py-2 text-sm">
-              Link1
+            <div className={linkStyle}>
+              <i className={"fab fa-instagram " + iconStyle}></i>
+              <i className={"fab fa-linkedin px-2 " + iconStyle}></i>
+              <i className={"fab fa-github " + iconStyle}></i>
             </div>
           </div>
-          <div className="table-row">
-            <div className="table-cell text-gray-700 px-4 py-2 text-xl">
-              <i className="fab fa-instagram"></i>
-              <i className="fab fa-linkedin px-2"></i>
-              <i className="fab fa-github"></i>
-            </div>
-            <div className="table-cell text-gray-700 px-4 py-2 text-sm">
-              Program2
-            </div>
-            <div className="table-cell text-gray-700 px-4 py-2 text-sm">
-              Link2
-            </div>
+        </div>
+        <div className="mt-8">
+          <span className={headingStyle}>Programs</span>
+          <div className="grid grid-cols-1">
+            <div className={linkStyle}>Program 1</div>
+            <div className={linkStyle}>Program 2</div>
+            <div className={linkStyle}>Program 3</div>
           </div>
-
-          <div className="table-row">
-            <div className="table-cell text-gray-700 px-4 py-2 text-sm">
-              © 2020 | All rights reserved
-            </div>
+        </div>
+        <div className="mt-8">
+          <span className={headingStyle}>External Links</span>
+          <div className="grid grid-cols-1">
+            <div className={linkStyle}>External 1</div>
+            <div className={linkStyle}>External 2</div>
+            <div className={linkStyle}>External 3</div>
           </div>
         </div>
       </div>
