5d33b53b3d27f2c10c36feb14b82ec260b62a9b0
Kytha <thatchek@mcmaster.ca>
2020-11-27 13:24:02 -0500

    Format Splash section spacing

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -26,12 +26,12 @@ const Splash = () => {
       {/* first row - splash content */}
       <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-24">
         {/* splash text */}
-        <div className="grid grid-rows-3">
-          <h1 className="text-5xl">
+        <div className="flex flex-col">
+          <h1 className="text-5xl mb-16">
             Developer Student
             <br /> Clubs McMaster U
           </h1>
-          <p className="text-gray-800">
+          <p className="text-gray-800 text-lg mb-16">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tortor
             ligula, hendrerit eu turpis id, feugiat mattis nulla. Integer
             pretium, sem id vestibulum vestibulum, magna est elementum mauris,
