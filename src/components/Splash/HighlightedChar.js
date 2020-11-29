beba53f1092c5b587b8a406d935de6b6b6f51e63
Kytha <thatchek@mcmaster.ca>
2020-11-28 19:16:54 -0500

    Adjust HighlightedChar color opacity

--- src/components/Splash/HighlightedChar.js
+++ src/components/Splash/HighlightedChar.js
@@ -2,10 +2,10 @@ import React from "react";
 import cx from "classnames";
 
 const highlights = {
-  red: { textShadow: "0 0 10px RGBA(234,67,53,0.5)" },
-  blue: { textShadow: "0 0 10px RGBA(66,133,244,0.5)" },
-  green: { textShadow: "0 0 10px RGBA(52,168,83,0.5)" },
-  yellow: { textShadow: "0 0 10px RGBA(251,188,4,0.5)" },
+  red: { textShadow: "0 0 10px RGBA(234,67,53,0.4)" },
+  blue: { textShadow: "0 0 10px RGBA(66,133,244,0.4)" },
+  green: { textShadow: "0 0 10px RGBA(52,168,83,0.4)" },
+  yellow: { textShadow: "0 0 10px RGBA(251,188,4,0.4)" },
 };
 
 const HighlightedChar = ({ color, children }) => {
