a5925e6977b679b415c5e6412b0723dfee0aef42
edwin-do <doedwin123@gmail.com>
2020-11-30 14:56:15 -0500

    format media.js

--- src/components/media.js
+++ src/components/media.js
@@ -3,16 +3,44 @@ import logo from "../images/icon.png";
 import cx from "classnames";
 
 const socials = [
-  { icon: "fab fa-facebook", url: "https://www.facebook.com/DSCMcMasterU/ ", color: "text-blue-800", hover: "hover:text-gray-500"},
-  { icon: "fab fa-instagram", url: "https://www.instagram.com/dscmcmasteru/", color: "text-pink-600", hover: "hover:text-gray-500"},
-  { icon: "fab fa-linkedin", url: "https://ca.linkedin.com/company/dscmcmasteru", color: "text-blue-600", hover: "hover:text-gray-500" },
-  { icon: "fab fa-twitter", url: "https://twitter.com/DSCMcMaster", color: "text-blue-400", hover: "hover:text-gray-500"},
-  { icon: "fab fa-github", url: "https://github.com/DSC-McMaster-U/", color: "text-gray-800", hover: "hover:text-gray-500"},
+  {
+    icon: "fab fa-facebook",
+    url: "https://www.facebook.com/DSCMcMasterU/ ",
+    color: "text-blue-800",
+    hover: "hover:text-gray-500",
+  },
+  {
+    icon: "fab fa-instagram",
+    url: "https://www.instagram.com/dscmcmasteru/",
+    color: "text-pink-600",
+    hover: "hover:text-gray-500",
+  },
+  {
+    icon: "fab fa-linkedin",
+    url: "https://ca.linkedin.com/company/dscmcmasteru",
+    color: "text-blue-600",
+    hover: "hover:text-gray-500",
+  },
+  {
+    icon: "fab fa-twitter",
+    url: "https://twitter.com/DSCMcMaster",
+    color: "text-blue-400",
+    hover: "hover:text-gray-500",
+  },
+  {
+    icon: "fab fa-github",
+    url: "https://github.com/DSC-McMaster-U/",
+    color: "text-gray-800",
+    hover: "hover:text-gray-500",
+  },
 ];
 
 function Media() {
   return (
-    <section id = "Media" className="relative mt-32 pt-32 mb-10 border-t-4 border-blue-400">
+    <section
+      id="Media"
+      className="relative mt-32 pt-32 mb-10 border-t-4 border-blue-400"
+    >
       <div className="container mx-auto">
         <div className="flex items-center flex-col">
           <div className="flex md:mb-12 mb-8">
@@ -31,7 +59,9 @@ function Media() {
               return (
                 <div className="px-10 text-6xl mb-5 md:mb-0">
                   <a href={social.url}>
-                    <i className={cx(social.icon, social.color, social.hover)}></i>
+                    <i
+                      className={cx(social.icon, social.color, social.hover)}
+                    ></i>
                   </a>
                 </div>
               );
