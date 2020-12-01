fa1997f2a386086691619bbc66fdb1ee9ac8f2b9
Tasmiha Hassan <htasmiha@gmail.com>
2020-12-01 17:51:33 -0500

    First final implementation of event cards

--- src/components/Events/EventCard.js
+++ src/components/Events/EventCard.js
@@ -1,15 +1,36 @@
 import React from "react";
-import thinking from "../../images/thinking.png";
 
-const EventCard = ({ title, image, body, link, date, location, time }) => {
-  image = thinking;
-  body =
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elitVoluptatibus quia, nulla! Maiores et perferendis eaque,exercitationem praesentium nihil.";
+const EventCard = ({
+  title,
+  image,
+  body,
+  link,
+  date,
+  location,
+  time,
+  jsDate,
+}) => {
   return (
     <div className="bg-white rounded-sm px-10 shadow-lg">
-      <div className="max-w-sm overflow-hidden h-auto">
+      <div className="max-w-sm overflow-hidden h-full">
+        <div className="pt-6 pb-2">
+          {new Date(jsDate) >= new Date() && (
+            <span className="rounded-lg bg-blue-400 text-white py-2 px-2 text-xs">
+              Upcoming Event
+            </span>
+          )}
+          {new Date(jsDate) < new Date() && (
+            <span className="rounded-lg bg-gray-500 text-white py-2 px-2 text-xs">
+              Past Event
+            </span>
+          )}
+        </div>
         <div className="flex justify-center">
-          <img className="block h-32 w-auto" src={image} alt="rocket" />
+          <img
+            className="block h-32 w-auto pt-2 pb-2"
+            src={image}
+            alt="rocket"
+          />
         </div>
         <div className="px-2 pb-4 text-center">
           {/* <div className="flex flex-row"> */}
@@ -31,7 +52,7 @@ const EventCard = ({ title, image, body, link, date, location, time }) => {
               {location}
             </span>
           </div>
-          <p className="relative body my-4 text-center text-sm leading-normal">
+          <p className="relative body my-4 text-left text-sm leading-normal">
             {body}
           </p>
         </div>
--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -55,15 +55,17 @@ function Events() {
                 spread="wide"
                 alignment="horizontal"
               >
-                {EventData.sort((a, b) => b.date - a.date)
-                  // .filter(event => {
-                  //   return new Date(event.jsDate) > new Date();
-                  // })
-                  .map((card, i) => {
+                {EventData.sort(
+                  (a, b) => new Date(b.jsDate) - new Date(a.jsDate)
+                )
+                  .filter((event, i) => {
+                    return i < maxEvents;
+                  })
+                  .map(card => {
                     return (
                       <EventCard
                         title={card.title}
-                        body={"card.body"}
+                        body={card.body}
                         image={card.image}
                         link={card.link}
                         date={card.date}
@@ -76,18 +78,17 @@ function Events() {
               </ReactCardCarousel>
             </div>
           </div>
-          {EventData.sort((a, b) => b.jsDate - a.jsDate)
-            // .filter((event, i) => {
-            //   return new Date(event.jsDate) > new Date();
-            // })
-            .map((card, i) => {
-              console.log(card);
+          {EventData.sort((a, b) => new Date(b.jsDate) - new Date(a.jsDate))
+            .filter((event, i) => {
+              return i < maxEvents;
+            })
+            .map(card => {
               return (
                 <div className="flex justify-center md:col-span-6 md:hidden">
                   <div className="relative h-full w-11/12 sm:w-8/12  md:w-full mt-20 md:mt-0  md:hidden">
                     <EventCard
                       title={card.title}
-                      body={"card.body"}
+                      body={card.body}
                       image={card.image}
                       link={card.link}
                       date={card.date}
@@ -101,46 +102,6 @@ function Events() {
             })}
         </div>
       </div>
-      {/* </div> */}
-      {/* <ul className="pt-8 md:pl-8">
-            {EventData.filter(event => {
-              return new Date(event.jsDate) > new Date();
-            }).map((event, i) => {
-              return (
-                i < maxEvents && (
-                  <li className={i === 0 ? "mx-auto" : "mx-auto pt-12"}>
-                    <div className="flex flex-row">
-                      <div>
-                        <img
-                          src={require("../../images/dscIcon.png")}
-                          alt="DSC Icon"
-                        />
-                      </div>
-                      <a href={event.link} className="pl-2 hover:underline">
-                        {event.title}
-                      </a>
-                    </div>
-                    <div className="flex flex-row pl-10 pt-2">
-                      <span className="text-gray-700 mr-2 text-xs">
-                        <i className="fill-current far fa-calendar-alt text-blue-400 fa-lg pt-2 pr-2"></i>
-                        {event.date}
-                      </span>
-                      <span className="text-gray-700 mr-2 text-xs">
-                        <i className="fill-current far fa-clock text-green-400 fa-lg pt-2 pr-2"></i>
-                        {event.time}
-                      </span>
-                      <span className="text-gray-700 mr-2 text-xs">
-                        <i className="fill-current fas fa-map-marker-alt text-red-400 fa-lg pt-2 pr-2"></i>
-                        {event.location}
-                      </span>
-                    </div>
-                  </li>
-                )
-              );
-            })}
-          </ul>
-        </div>
-      </div> */}
     </section>
   );
 }
