fed0851a0378d2249d2b8c0043dca0c2a9823a05
wuyachen <1774288078@qq.com>
2020-12-01 18:20:49 -0500

    add links and hover footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const colStyle = "mt-8 md:ml-4";
 const headingStyle = "px-4 py-2 text-lg text-gray-800 font-semibold";
-const linkStyle = "text-gray-600 px-4 py-2 text-sm";
+const linkStyle = "text-gray-600 px-4 py-2 text-sm cursor-pointer hover:text-indigo-900";
 const iconStyle = "text-xl";
 
 function Footer() {
@@ -12,7 +12,7 @@ function Footer() {
         <div className={colStyle}>
           <span className={headingStyle}>Contact Us</span>
           <div className="grid grid-cols-1">
-            <div className={linkStyle}>© 2020 | All rights reserved</div>
+            <div className="text-gray-600 px-4 py-2 text-sm">© 2020 | All rights reserved</div>
             <div className={linkStyle}>
               Reach out to us at dscmcmaster@email.ca
             </div>
@@ -26,25 +26,23 @@ function Footer() {
         <div className={colStyle}>
           <span className={headingStyle}>Programs</span>
           <div className="grid grid-cols-1">
-            <div className={linkStyle}>Incubator</div>
-            <div className={linkStyle}>Program 2</div>
-            <div className={linkStyle}>Program 3</div>
+            <div className={linkStyle}><a href="https://forms.gle/wsuNPVfvNYW6PQqN7">Incubator</a></div>
           </div>
         </div>
         <div className={colStyle}>
           <span className={headingStyle}>Developers</span>
           <div className="grid grid-cols-1">
-            <div className={linkStyle}>Firebase console</div>
-            <div className={linkStyle}>Google Cloud Platform</div>
-            <div className={linkStyle}>Actions on Google</div>
+            <div className={linkStyle}><a href= "https://firebase.google.com/">Firebase console</a></div>
+            <div className={linkStyle}><a href= "https://cloud.google.com/">Google Cloud Platform</a></div>
+            <div className={linkStyle}><a href= "https://news.google.com/">Actions on Google</a></div>
           </div>
         </div>
         <div className={colStyle}>
           <span className={headingStyle}>External Links</span>
           <div className="grid grid-cols-1">
-            <div className={linkStyle}>MLH Hackathons</div>
-            <div className={linkStyle}>Devpost</div>
-            <div className={linkStyle}>Wiki</div>
+            <div className={linkStyle}><a href= "https://mlh.io/">MLH Hackathons</a></div>
+            <div className={linkStyle}><a href= "https://devpost.com/">Devpost</a></div>
+            <div className={linkStyle}><a href= "https://www.notion.so/DSC-McMasterU-Wiki-f39c9ea5e7b141fba08cfc01588d92c4">Wiki</a></div>
           </div>
         </div>
       </div>
