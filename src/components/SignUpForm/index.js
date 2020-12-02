6a48fc5f6d6eae9a679527d899a2b9b842a29485
Kytha <thatchek@mcmaster.ca>
2020-12-01 19:53:50 -0500

    Typography and layout standardized

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -5,8 +5,11 @@ import Button from "../Button";
 import { ACADEMIC_YEARS, TEAMS } from "./config";
 import Reaptcha from "reaptcha";
 
-
-const discord = { icon: "fab fa-discord", url: "https://discord.com/invite/KxUCq7U", color: "text-blue-400" }
+const discord = {
+  icon: "fab fa-discord",
+  url: "https://discord.com/invite/KxUCq7U",
+  color: "text-blue-400",
+};
 
 const formUrl =
   "https://script.google.com/macros/s/AKfycbxaAM3uyL_avJPMm4SsjXUVs-TqorvKLFohkKy2cP1J2hZ14ZQ3/exec";
@@ -34,33 +37,35 @@ const labelClasses = {
   base: ["text-lg"],
 };
 const ThankYou = () => (
-  <div>
-    <p>Thank You! You will receive an email from us shortly with more details.</p>
-    <br></br>
-    <p>Please request acceptance to our community discord channel here.</p>
-    <div className="px-10 text-6xl mb-5 md:mb-0">
+  <div className="mb-10">
+    <h1 className="text-5xl mb-5">Thank You!</h1>
+    <p className="text-gray-700 leading-relaxed mb-8">
+      You will receive an email from us shortly with more details. Please
+      request acceptance to our community discord channel here.
+    </p>
+    <div className="text-6xl md:mb-0">
       <a href={discord.url}>
-        <i className={cx(discord.icon, discord.color)}></i>
+        <i
+          className={cx(discord.icon, discord.color, "hover:text-blue-600")}
+        ></i>
       </a>
-    </div>      
+    </div>
   </div>
 );
 
 const FailedSubmission = () => (
-  <h1 className="text-red-800 text-xl">
+  <p className="text-gray-700 text-xl leading-loose">
     Something happened and your application was not submitted. Please try again
-    or use our old form.
-    <br></br>
-    <br></br>
+    or use our
     <a
       href="https://docs.google.com/forms/d/e/1FAIpQLScjcm8b9ay0-JFcANSH_-A19zO7-KZK40_ppwO2Pno88eTh7A/viewform"
-      className="text-blue-800 text-xl"
+      className="text-blue-800 underline"
       target="_blank"
       rel="noopener noreferrer"
     >
       Old form
     </a>
-  </h1>
+  </p>
 );
 
 const InputField = ({
@@ -138,18 +143,18 @@ export default class SignUp extends Component {
     this.captcha.renderExplicitly();
   };
 
-  onVerify = () => {  
+  onVerify = () => {
     this.setState({
       notABot: true,
     });
   };
 
   onExpire = () => {
-    this.captcha.reset()
+    this.captcha.reset();
 
     this.setState({
       notABot: false,
-    });    
+    });
   };
 
   onChange = e => {
@@ -231,166 +236,168 @@ export default class SignUp extends Component {
     } = this.state;
     return (
       <div>
-        {submitted ? (
+        {!submitted ? (
           <div>{errorFound ? <FailedSubmission /> : <ThankYou />}</div>
         ) : (
           <div>
-            <h1 className="font-bold">Join us!</h1>
-            <p className="mb-16">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Pellentesque lorem mauris, posuere id lectus ut, dapibus porttitor
-              lacus. Cras rhoncus id augue nec rutrum. Nullam tellus sem,
-              pretium et sodales in, convallis sit amet erat. Aliquam erat
-              volutpat.
+            <h1 className="text-5xl mb-5">Join us!</h1>
+            <p className="mb-16 leading-relaxed text-gray-700">
+              To become a general member, complete and submit the form below. By
+              submitting this form you will indicate which of our 5 teams you
+              prefer to be placed in, and will be invited to join our Discord
+              server. By being a general member, you will get the chance to work
+              with the team behind DSC McMaster U and help organize club events
+              and activities! You'll join a community of about 300 students all
+              with the same passion of technology and coding.
             </p>
-            <div className="lg:w-3/4">
-              <form
-                className="flex flex-wrap rounded overflow-hidden shadow-2xl p-4"
-                onSubmit={this.onSubmit}
-              >
-                <div className="w-full md:w-3/4 px-4 mb-8">
-                  <InputField
-                    name="email"
-                    value={email}
-                    label="Email"
-                    placeholder="@gmail.com, @mcmaster.ca, etc."
-                    onChange={this.onChange}
-                    required
-                  />
-                </div>
-                <div className="w-full md:w-1/2 px-4 mb-8">
-                  <InputField
-                    name="first_name"
-                    label="First name"
-                    value={first_name}
-                    onChange={this.onChange}
-                    required
-                  />
-                </div>
-                <div className="w-full md:w-1/2 px-4 mb-8">
-                  <InputField
-                    name="last_name"
-                    label="Last name"
-                    value={last_name}
-                    onChange={this.onChange}
-                    required
-                  />
-                </div>
-                <div className="w-full md:w-3/4 px-4 mb-8">
-                  <InputField
-                    name="mac_id"
-                    label="Mac ID"
-                    value={mac_id}
-                    placeholder="e.g. muske7"
-                    onChange={this.onChange}
-                    required
-                  />
-                </div>
-                <div className="w-full md:w-1/3 px-4 mb-8">
-                  <InputField
-                    name="program"
-                    label="Program"
-                    value={program}
-                    onChange={this.onChange}
-                    required
-                  />
-                </div>
-                <div className="w-full md:w-1/3 px-4 mb-8">
-                  <Select
-                    name="academic_year"
-                    label="Academic year"
-                    value={academic_year}
-                    onChange={this.onChange}
-                    required
-                  >
-                    {ACADEMIC_YEARS.map(academic_year => (
-                      <option key={academic_year}>{academic_year}</option>
-                    ))}
-                  </Select>
-                </div>
-                <div className="w-full md:w-1/3 px-4 mb-8">
-                  <InputField
-                    name="graduation"
-                    label="Expected graduation"
-                    value={graduation}
-                    placeholder="e.g. 2023"
-                    onChange={this.onChange}
-                    required
-                  />
-                </div>
-                <div className="w-full text-xl bg-blue-500 shadow-xl text-white mt-8 mb-12">
-                  <p className="text-center m-auto p-8">
-                    Select your top three team preferences
-                  </p>
-                </div>
-                <div className="w-full md:w-1/3 px-4 mb-16">
-                  <Select
-                    name="first_choice_team"
-                    label="1st choice"
-                    value={first_choice_team}
-                    onChange={this.onChange}
-                  >
-                    {TEAMS.map(team => (
-                      <option key={team} value={team}>
-                        {team}
-                      </option>
-                    ))}
-                  </Select>
-                </div>
-                <div className="w-full md:w-1/3 px-4 mb-16">
-                  <Select
-                    name="second_choice_team"
-                    label="2nd choice"
-                    value={second_choice_team}
-                    onChange={this.onChange}
-                  >
-                    {TEAMS.map(team => (
-                      <option key={team} value={team}>
-                        {team}
-                      </option>
-                    ))}
-                  </Select>
-                </div>
-                <div className="w-full md:w-1/3 px-4 mb-16">
-                  <Select
-                    name="third_choice_team"
-                    label="3rd choice"
-                    value={third_choice_team}
-                    onChange={this.onChange}
-                  >
-                    {TEAMS.map(team => (
-                      <option key={team} value={team}>
-                        {team}
-                      </option>
-                    ))}
-                  </Select>
-                </div>
-
-                <div className="ml-16 w-4/9 mt-4">
-                  <Reaptcha
-                    ref={e => (this.captcha = e)}
-                    sitekey="6LceNOQZAAAAAAkq5YAljpujgj0pujsJ9pnKL6Sw"
-                    onLoad={this.onLoad}
-                    onVerify={this.onVerify}
-                    onExpire={this.onExpire}
-                    explicit
-                  />
-                </div>
-                
-                <Button
-                  bg="green"
-                  type="submit"
-                  size="lg"
-                  className="mr-auto sm:ml-4 w-11/12 md:ml-8 md:w-5/12 lg:w-5/12 xl:w-5/12 px-16 mt-8 mb-2"
-                  disabled={this.state.notABot ? false : true}                  
-                  float
+            <div className="lg:w-3/4 mx-auto">
+              <div className="shadow-2xl px-4 py-8">
+                <h2 className="text-3xl mb-8 px-4">General Member Form</h2>
+                <form
+                  className="flex flex-wrap rounded overflow-hidden"
+                  onSubmit={this.onSubmit}
                 >
-                  Submit
-                </Button>
-                
+                  <div className="w-full md:w-3/4 px-4 mb-8">
+                    <InputField
+                      name="email"
+                      value={email}
+                      label="Email"
+                      placeholder="@gmail.com, @mcmaster.ca, etc."
+                      onChange={this.onChange}
+                      required
+                    />
+                  </div>
+                  <div className="w-full md:w-1/2 px-4 mb-8">
+                    <InputField
+                      name="first_name"
+                      label="First name"
+                      value={first_name}
+                      onChange={this.onChange}
+                      required
+                    />
+                  </div>
+                  <div className="w-full md:w-1/2 px-4 mb-8">
+                    <InputField
+                      name="last_name"
+                      label="Last name"
+                      value={last_name}
+                      onChange={this.onChange}
+                      required
+                    />
+                  </div>
+                  <div className="w-full md:w-3/4 px-4 mb-8">
+                    <InputField
+                      name="mac_id"
+                      label="Mac ID"
+                      value={mac_id}
+                      placeholder="e.g. muske7"
+                      onChange={this.onChange}
+                      required
+                    />
+                  </div>
+                  <div className="w-full md:w-1/3 px-4 mb-8">
+                    <InputField
+                      name="program"
+                      label="Program"
+                      value={program}
+                      onChange={this.onChange}
+                      required
+                    />
+                  </div>
+                  <div className="w-full md:w-1/3 px-4 mb-8">
+                    <Select
+                      name="academic_year"
+                      label="Academic year"
+                      value={academic_year}
+                      onChange={this.onChange}
+                      required
+                    >
+                      {ACADEMIC_YEARS.map(academic_year => (
+                        <option key={academic_year}>{academic_year}</option>
+                      ))}
+                    </Select>
+                  </div>
+                  <div className="w-full md:w-1/3 px-4 mb-8">
+                    <InputField
+                      name="graduation"
+                      label="Expected graduation"
+                      value={graduation}
+                      placeholder="e.g. 2023"
+                      onChange={this.onChange}
+                      required
+                    />
+                  </div>
+                  <div className="w-full text-xl bg-blue-500 shadow-xl text-white mt-8 mb-12">
+                    <p className="text-center m-auto p-8">
+                      Select your top three team preferences
+                    </p>
+                  </div>
+                  <div className="w-full md:w-1/3 px-4 mb-16">
+                    <Select
+                      name="first_choice_team"
+                      label="1st choice"
+                      value={first_choice_team}
+                      onChange={this.onChange}
+                    >
+                      {TEAMS.map(team => (
+                        <option key={team} value={team}>
+                          {team}
+                        </option>
+                      ))}
+                    </Select>
+                  </div>
+                  <div className="w-full md:w-1/3 px-4 mb-16">
+                    <Select
+                      name="second_choice_team"
+                      label="2nd choice"
+                      value={second_choice_team}
+                      onChange={this.onChange}
+                    >
+                      {TEAMS.map(team => (
+                        <option key={team} value={team}>
+                          {team}
+                        </option>
+                      ))}
+                    </Select>
+                  </div>
+                  <div className="w-full md:w-1/3 px-4 mb-16">
+                    <Select
+                      name="third_choice_team"
+                      label="3rd choice"
+                      value={third_choice_team}
+                      onChange={this.onChange}
+                    >
+                      {TEAMS.map(team => (
+                        <option key={team} value={team}>
+                          {team}
+                        </option>
+                      ))}
+                    </Select>
+                  </div>
 
-                
-              </form>
+                  <div className="ml-16 w-4/9 mt-4">
+                    <Reaptcha
+                      ref={e => (this.captcha = e)}
+                      sitekey="6LceNOQZAAAAAAkq5YAljpujgj0pujsJ9pnKL6Sw"
+                      onLoad={this.onLoad}
+                      onVerify={this.onVerify}
+                      onExpire={this.onExpire}
+                      explicit
+                    />
+                  </div>
+
+                  <Button
+                    bg="green"
+                    type="submit"
+                    size="lg"
+                    className="mr-auto sm:ml-4 w-11/12 md:ml-8 md:w-5/12 lg:w-5/12 xl:w-5/12 px-16 mt-8 mb-2"
+                    disabled={this.state.notABot ? false : true}
+                    float
+                  >
+                    Submit
+                  </Button>
+                </form>
+              </div>
             </div>
           </div>
         )}
