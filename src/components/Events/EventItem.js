5bfa6de205b0f0fc6befdf56da0aab649dd3d705
Kishor Pandya <77642092+kishorpandya2210@users.noreply.github.com>
2022-11-27 11:24:15 -0500

    Feat/events redesign2 (#72)
    * updated card view with sort by date
    
    * minor change
    
    * changed event cards
    
    Now event cards move on hover and you can click anywhere on the card to activate the link
    
    * Update Event “cool-event”
    
    * UI changes to events
    
    * removed some files
    
    Co-authored-by: Adarsh Patel <swe.adarsh@gmail.com>

--- src/components/Events/EventItem.js
+++ src/components/Events/EventItem.js
@@ -3,29 +3,31 @@ import dscIcon from "../../../static/images/dscIcon.png";
 import { prettifyDate } from "../../utils/date";
 const EventItem = ({ title, date, time, location, link }) => {
   return (
-    <li className="mx-auto pb-12" key={title}>
-      <div className="flex flex-row">
-        <div>
-          <img src={dscIcon} alt="DSC Icon" />
-        </div>
-        <a href={link} className="pl-2 hover:underline">
+    <li className="mx-auto w-full" key={title}>
+    <a href={link}>
+      <div className="h-auto w-full rounded-lg bg-gray-100 bg-opacity-20  shadow-lg text-grey px-2 py-8 hover:translate-x-2">
+        <div className="flex flex-row">
+          <div className="mr-2">
+            <img src={dscIcon} alt="DSC Icon" />
+          </div>
           {title}
-        </a>
+        </div>
+        <div className="flex flex-row pl-10 pt-2">
+          <span className="text-gray-900 mr-2 text-xs">
+            <i className="fill-current far fa-calendar-alt text-blue-400 fa-lg pt-2 pr-2"></i>
+            {prettifyDate(date)}
+          </span>
+          <span className="text-gray-900 mr-2 text-xs">
+            <i className="fill-current far fa-clock text-green-400 fa-lg pt-2 pr-2"></i>
+            {time}
+          </span>
+          <span className="text-gray-00 mr-2 text-xs">
+            <i className="fill-current fas fa-map-marker-alt text-red-400 fa-lg pt-2 pr-2"></i>
+            {location}
+          </span>
+        </div>
       </div>
-      <div className="flex flex-row pl-10 pt-2">
-        <span className="text-gray-600 mr-2 text-xs">
-          <i className="fill-current far fa-calendar-alt text-blue-400 fa-lg pt-2 pr-2"></i>
-          {prettifyDate(date)}
-        </span>
-        <span className="text-gray-600 mr-2 text-xs">
-          <i className="fill-current far fa-clock text-green-400 fa-lg pt-2 pr-2"></i>
-          {time}
-        </span>
-        <span className="text-gray-600 mr-2 text-xs">
-          <i className="fill-current fas fa-map-marker-alt text-red-400 fa-lg pt-2 pr-2"></i>
-          {location}
-        </span>
-      </div>
+      </a>
     </li>
   );
 };
