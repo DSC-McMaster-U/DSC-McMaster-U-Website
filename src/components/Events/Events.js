b696a81d9c29c342fdaac482279c1cc636dae20f
kishorpandya2210 <kishorpandya17@gmail.com>
2022-11-26 19:56:37 -0500

    UI changes to events

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -9,57 +9,57 @@ import Typography from "../core/Typography";
 
 function Events() {
   const maxEvents = 6;
-  const testdata = [
-    {
-      title: "Test Event1",
-      date: "2022-12-3",
-      time: "6:00PM",
-      location: "Virtual",
-      link: "https://www.google.ca",
-    },
-    {
-      title: "Test Event2",
-      date: "2022-12-4",
-      time: "6:00PM",
-      location: "Virtual",
-      link: "www.google.ca",
-    },
-    {
-      title: "Test Event3",
-      date: "2022-12-5",
-      time: "6:00PM",
-      location: "Virtual",
-      link: "www.google.ca",
-    },
-    {
-      title: "Test Event4",
-      date: "2022-12-6",
-      time: "6:00PM",
-      location: "Virtual",
-      link: "www.google.ca",
-    },
-    {
-      title: "Test Event5",
-      date: "2022-12-7",
-      time: "6:00PM",
-      location: "Virtual",
-      link: "www.google.ca",
-    },
-    {
-      title: "Test Event6",
-      date: "2022-12-8",
-      time: "6:00PM",
-      location: "Virtual",
-      link: "www.google.ca",
-    },
-    {
-      title: "Test Event X",
-      date: "2022-12-1",
-      time: "6:00PM",
-      location: "Virtual",
-      link: "www.google.ca",
-    },
-  ];
+  // const testdata = [
+  //   {
+  //     title: "Test Event1",
+  //     date: "2022-12-3",
+  //     time: "6:00PM",
+  //     location: "Virtual",
+  //     link: "https://www.google.ca",
+  //   },
+  //   {
+  //     title: "Test Event2",
+  //     date: "2022-12-4",
+  //     time: "6:00PM",
+  //     location: "Virtual",
+  //     link: "www.google.ca",
+  //   },
+  //   {
+  //     title: "Test Event3",
+  //     date: "2022-12-5",
+  //     time: "6:00PM",
+  //     location: "Virtual",
+  //     link: "www.google.ca",
+  //   },
+  //   {
+  //     title: "Test Event4",
+  //     date: "2022-12-6",
+  //     time: "6:00PM",
+  //     location: "Virtual",
+  //     link: "www.google.ca",
+  //   },
+  //   {
+  //     title: "Test Event5",
+  //     date: "2022-12-7",
+  //     time: "6:00PM",
+  //     location: "Virtual",
+  //     link: "www.google.ca",
+  //   },
+  //   {
+  //     title: "Test Event6",
+  //     date: "2022-12-8",
+  //     time: "6:00PM",
+  //     location: "Virtual",
+  //     link: "www.google.ca",
+  //   },
+  //   {
+  //     title: "Test Event X",
+  //     date: "2022-12-1",
+  //     time: "6:00PM",
+  //     location: "Virtual",
+  //     link: "www.google.ca",
+  //   },
+  // ];
   const data = useStaticQuery(graphql`
     query EventsImage {
       image: file(relativePath: { eq: "events.png" }) {
@@ -99,7 +99,7 @@ function Events() {
         </div>
         <div className="py-2 w-full">
           <ul className="pt-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:pl-8 pb-12">
-            {/* {EventData.filter(edge => {
+            {EventData.filter(edge => {
               return stringToDate(edge.node.date) >= new Date();
             })
               .sort(
@@ -109,9 +109,9 @@ function Events() {
               )
               .map((edge, i) => {
                 return i < maxEvents && <EventItem {...edge.node} key={i} />;
-              })} */}
+              })}
 
-            {testdata
+            {/* {testdata
               .filter(edge => {
                 return stringToDate(edge.date) >= new Date();
               })
@@ -122,7 +122,7 @@ function Events() {
               )
               .map((edge, i) => {
                 return i < maxEvents && <EventItem {...edge} key={i} />;
-              })}
+              })} */}
           </ul>
         </div>
         <div className="self-baseline">
